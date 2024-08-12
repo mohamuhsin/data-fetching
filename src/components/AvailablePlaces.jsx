2533cc1713c7822464f1683332579deb823a7eb6
mohamuhsin <muhsinohoho@outlook.com>
2024-08-12 19:19:05 +0300

    created HTTP request to get data from our server using the fetch method, but our request has a problem

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,22 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+import Places from "./Places.jsx";
+import { response } from "express";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
