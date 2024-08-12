b895739f57065d2cf4d02d538351454d64497588
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-08-13 00:07:45 +0300

    http request with fetch api to fetch available places from backend

--- backend/app.js
+++ backend/app.js
@@ -50,4 +50,4 @@ app.use((req, res, next) => {
   res.status(404).json({ message: '404 - Not Found' });
 });
 
-app.listen(3000);
+app.listen(3000,()=>{console.log('Server started on port 3000')});
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,21 @@
-import Places from './Places.jsx';
+import {useEffect, useState} from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const[availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places").then((response) => {
+      return response.json()
+    }).then((resData)=>{
+      console.log(resData.places);
+      setAvailablePlaces(resData.places)
+    })
+  }, []);
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
