c176ec4c535852b71e42074662149d15590e7022
mohamuhsin <muhsinohoho@outlook.com>
2024-08-14 20:12:01 +0300

    used optistic updating to update our places and also handled error

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
 import { updateUserPlaces } from "./https.js";
+import Error from "./components/Error.jsx";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -37,7 +39,10 @@ function App() {
     try {
       await updateUserPlaces([selectedPlace, ...userPlaces]);
     } catch (error) {
-      //...
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || "Failed to update places.",
+      });
     }
   }
 
@@ -49,8 +54,21 @@ function App() {
     setModalIsOpen(false);
   }, []);
 
+  function handleError() {
+    setErrorUpdatingPlaces(null);
+  }
+
   return (
     <>
+      {errorUpdatingPlaces && (
+        <Modal open={errorUpdatingPlaces} onClose={handleError}>
+          <Error
+            title="An Error Occurred!"
+            message={errorUpdatingPlaces.message}
+            onConfirm={handleError}
+          />
+        </Modal>
+      )}
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
